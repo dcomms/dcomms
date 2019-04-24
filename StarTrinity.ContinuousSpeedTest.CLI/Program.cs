78fe255e42cb16576c04f5f76368dfdf0cdb7dc5
Sergey <asv@startrinity.com>
2019-04-24 19:01:41 +0300

    added CLI SUBT project

---
+++ StarTrinity.ContinuousSpeedTest.CLI/Program.cs
@@ -0,0 +1,69 @@
+﻿using Dcomms.P2PTP.LocalLogic;
+using Dcomms.SUBT;
+using System;
+using System.Net;
+
+namespace StarTrinity.ContinuousSpeedTest.CLI
+{
+    class Program
+    {
+        class User : ILocalPeerUser
+        {
+            bool ILocalPeerUser.EnableLog => true;
+            void ILocalPeerUser.WriteToLog(string message)
+            {
+             //   Console.WriteLine(message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            var coordinatorServerIp1 = IPAddress.Parse("163.172.210.13");//neth3
+            var coordinatorServerIp2 = IPAddress.Parse("195.154.173.208");//fra2
+            var subtLocalPeer = new SubtLocalPeer(new SubtLocalPeerConfiguration
+            {
+                SenderThreadsCount = 4,
+                BandwidthTargetMbps = null,
+            });
+            var node = new LocalPeer(new LocalPeerConfiguration
+            {
+                RoleAsUser = true,
+                LocalPeerUser = new User(),
+                LocalUdpPortRangeStart = null,
+                SocketsCount = 4,
+                Coordinators = new IPEndPoint[]
+                {
+                    new IPEndPoint(coordinatorServerIp1, 10000),
+                    new IPEndPoint(coordinatorServerIp1, 10001),
+                    new IPEndPoint(coordinatorServerIp1, 10002),
+                    new IPEndPoint(coordinatorServerIp1, 10003),
+                    new IPEndPoint(coordinatorServerIp1, 10004),
+                    new IPEndPoint(coordinatorServerIp1, 10005),
+                    new IPEndPoint(coordinatorServerIp1, 10006),
+                    new IPEndPoint(coordinatorServerIp1, 10007),
+                    new IPEndPoint(coordinatorServerIp1, 9000),
+                    new IPEndPoint(coordinatorServerIp1, 9001),
+                    new IPEndPoint(coordinatorServerIp1, 9002),
+                    new IPEndPoint(coordinatorServerIp1, 9003),
+                    new IPEndPoint(coordinatorServerIp2, 9000),
+                    new IPEndPoint(coordinatorServerIp2, 9001),
+                    new IPEndPoint(coordinatorServerIp2, 9002),
+                    new IPEndPoint(coordinatorServerIp2, 9003),
+                },
+                Extensions = new[]
+                {
+                    subtLocalPeer
+                }
+            });
+            subtLocalPeer.MeasurementsHistory.OnAddedNewMeasurement += MeasurementsHistory_OnAddedNewMeasurement;
+
+            Console.WriteLine("running test...");
+            Console.ReadLine();
+            node.Dispose();
+        }
+
+        private static void MeasurementsHistory_OnAddedNewMeasurement(SubtMeasurement measurement)
+        {
+            Console.WriteLine($"measurement: download={measurement.RxBandwidthString}, upload={measurement.TxBandwidthString}");
+        }
+    }
+}
