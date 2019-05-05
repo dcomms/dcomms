e48a2a349b51251a9721e7456d4b596a83f2b4df
Sergey <asv@startrinity.com>
2019-05-05 12:41:05 +0300

    .

---
+++ StarTrinity.ContinuousSpeedTest.Xamarin/StarTrinity.ContinuousSpeedTest.Xamarin/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StarTrinity.ContinuousSpeedTest.Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        MainViewModel _mainVM;
+        public MainPage()
+        {
+            _mainVM = new MainViewModel();
+            InitializeComponent();
+            this.BindingContext = _mainVM;
+        }
+    }
+}
