a82b6898ab96d7a38e0c3be94f398140cff77375
Sergey <asv@startrinity.com>
2019-06-12 19:44:44 +0300

    first PoW is developed

--- Dcomms.CryptographyTester/CryptographyTester.cs
+++ Dcomms.CryptographyTester/CryptographyTester.cs
@@ -46,5 +46,34 @@ namespace Dcomms.CryptographyTester
             var callsPerSec = (double)n / sw.Elapsed.TotalSeconds;
             MessageBox.Show($"calls per sec: {callsPerSec}");
         });
+
+        public DelegateCommand TestPoW_sha1906 => new DelegateCommand(() =>
+        {
+            var sw = Stopwatch.StartNew();
+            var input = new byte[32];
+            var rnd = new Random();
+
+            const int indexStart = 4;
+            const int indexEnd = 6;
+            const byte value = 7;
+
+            int i = 0;
+            for (; ; i++)
+            {
+                rnd.NextBytes(input);
+                var r = _cryptoLibrary.GetHashSHA512(input);
+                bool ok = true;
+                for (int j = indexStart; j < indexEnd; j++)
+                    if (r[j] != value)
+                    {
+                        ok = false;
+                        break;
+                    }
+                if (ok) break;
+            }
+            sw.Stop();
+         
+            MessageBox.Show($"{i} loops; {sw.Elapsed.TotalMilliseconds}ms");
+        });
     }
 }
