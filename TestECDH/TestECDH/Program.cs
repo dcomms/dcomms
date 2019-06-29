f6f45c7841564c41a034c59a8051bc128a365c63
Sergey <asv@startrinity.com>
2019-06-29 13:16:06 +0300

    ECDH tests

---
+++ TestECDH/TestECDH/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using TestECDH.Lib;
+
+namespace TestECDH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = new Test1(x => Console.WriteLine(x));
+            test1.Test1_1();
+            test1.Test1_2();
+        }
+    }
+
+}
