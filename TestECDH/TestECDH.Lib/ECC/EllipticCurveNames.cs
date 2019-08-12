20135c7a0a2454519eff16ef767f9c1381ad2d5a
Sergey <asv@startrinity.com>
2019-08-12 12:30:56 +0300

    code cleaning

--- TestECDH/TestECDH.Lib/ECC/EllipticCurveNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EllipticCurveCrypto
-{
-    public static class EllipticCurveNames
-    {
-        public const string Secp256K1 = "secp256k1";
-        public const string Secp256R1 = "secp256r1";
-    }
-}
