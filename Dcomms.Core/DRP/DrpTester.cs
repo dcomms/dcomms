af7a5e0f381398a212d8cb559d99a6d01ec7dae8
Sergey <asv@startrinity.com>
2019-08-21 23:06:05 +0300

    more work on DRP

---
+++ Dcomms.Core/DRP/DrpTester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dcomms.DRP
+{
+    class DrpTester
+    {
+        public DrpTester()
+        {
+        }
+            
+    
+    }
+}
