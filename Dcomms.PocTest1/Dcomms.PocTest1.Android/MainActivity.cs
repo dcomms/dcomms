d3afbcb808250899c99a9c33e998564cfbe40e98
Sergey <asv@startrinity.com>
2019-11-27 22:12:11 +0300

    android PoC TDD: battery optimization

--- Dcomms.PocTest1/Dcomms.PocTest1.Android/MainActivity.cs
+++ Dcomms.PocTest1/Dcomms.PocTest1.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace Dcomms.PocTest1.Droid
 {
@@ -22,13 +23,28 @@ namespace Dcomms.PocTest1.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
-           MainService.StartService(this, (poc1)=>
-           {
-               LoadApplication(new App(poc1));
-           });
+            RequestToIgnoreBatteryOptimizations();
 
+            MainService.StartService(this, (poc1)=>
+            {
+                LoadApplication(new App(poc1));
+            });
+        }
+        void RequestToIgnoreBatteryOptimizations()
+        {
+            var pm = (PowerManager)Application.Context.GetSystemService(Context.PowerService);
+
+            if (!pm.IsIgnoringBatteryOptimizations(this.PackageName))
+            {              
+                var myIntent = new Intent();
+                myIntent.SetAction(Android.Provider.Settings.ActionIgnoreBatteryOptimizationSettings);
+                StartActivity(myIntent);
+            }
 
         }
+
+
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
