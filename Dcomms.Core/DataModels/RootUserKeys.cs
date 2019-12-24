9cef0f8055c439619ad35d15b17b455d618a5e42
Sergey <asv@startrinity.com>
2019-12-24 22:38:08 +0300

    development

---
+++ Dcomms.Core/DataModels/RootUserKeys.cs
@@ -0,0 +1,22 @@
+﻿using Dcomms.DMP;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dcomms.DataModels
+{
+    [Table("RootUserKeys")]
+    public class RootUserKeys
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+               
+        public int UserId { get; set; } // FK to Users table
+        
+        [Ignore]
+        public UserRootPrivateKeys UserRootPrivateKeys { get; set; }
+        public byte[] UserRootPrivateKeys_encrypted { get; set; }
+        public byte[] UserRootPrivateKeys_hmac { get; set; }
+    }
+}
---
+++ Dcomms.Core/DataModels/User.cs
@@ -0,0 +1,39 @@
+﻿using Dcomms.DMP;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dcomms.DataModels
+{
+
+    /// <summary>
+    /// both local user IDs and remote user IDs (=contact book)
+    /// </summary>
+    [Table("Users")]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Ignore]
+        public UserId UserID { get; set; }
+        public byte[] UserID_encrypted { get; set; }
+        public byte[] UserID_hmac { get; set; }
+
+        [Ignore]
+        public string AliasID { get; set; }
+        public byte[] AliasID_encrypted { get; set; }
+        public byte[] AliasID_hmac { get; set; }
+
+        [Ignore]
+        ///////////////////////////////////[SQLite.Indexed]
+        public bool IsLocal => LocalUserCertificate_encrypted != null;
+        [Ignore]
+        public UserCertificate LocalUserCertificate { get; set; }
+        public byte[] LocalUserCertificate_encrypted { get; set; }
+        public byte[] LocalUserCertificate_hmac { get; set; }
+
+        public override string ToString() => AliasID;
+    }
+}
