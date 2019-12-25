2cb343cbbf1002f621244e2d68cb63e35695ab3b
Sergey <asv@startrinity.com>
2019-12-25 23:49:07 +0300

    development

---
+++ Dcomms.MessengerT/Controllers/LocalUsersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dcomms.MessengerT.Controllers
+{
+    public class LocalUsersController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            var localUser = Program.UserAppEngine.LocalUsers.FirstOrDefault(x => x.User.Id == id);
+            if (localUser == null) return NotFound();
+            return View(localUser);
+        }
+    }
+}
