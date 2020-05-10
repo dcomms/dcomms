c846a3d887773785bf6ef3bbe6b48456fd19db04
Sergey <asv@startrinity.com>
2020-05-10 12:04:38 +0300

    .

---
+++ StarTrinity.ContinuousSpeedTest/tt/VersionAndCompiledDateCs.cs
@@ -0,0 +1,28 @@
+﻿ 
+namespace StarTrinity.ContinuousSpeedTest
+{	
+		public static class CompilationInfo
+		{
+			public static uint CompilationDateTimeUtc_uint32
+			{
+				get
+				{
+					return 42449540;
+				}
+			}
+			public static System.DateTime CompilationDateTimeUtc
+			{
+				get
+				{
+					return Dcomms.MiscProcedures.Uint32secondsToDateTime(CompilationDateTimeUtc_uint32);
+				}
+			}
+			public static string CompilationDateTimeUtcStr
+			{
+				get
+				{
+					return string.Format("{0:yyMMdd-HH:mm}", CompilationDateTimeUtc);
+				}
+			}
+		}
+}
